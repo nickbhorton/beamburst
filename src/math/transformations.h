828fe6494c2e220163f0c7ef049835e389f8904e
Nick Horton <nickkbhorton@gmail.com>
2024-04-25 02:07:08 -0500

    added translate matrix

--- src/math/transformations.h
+++ src/math/transformations.h
@@ -20,6 +20,30 @@ auto scaling_matrix(T scalar) -> std::array<std::array<T, 4>, 4>
     };
 }
 
+template <typename T>
+auto translation_matrix(std::array<T, 3> translation_vector)
+    -> std::array<std::array<T, 4>, 4>
+{
+    return std::array<std::array<T, 4>, 4>{
+        {{static_cast<T>(1),
+          static_cast<T>(0),
+          static_cast<T>(0),
+          translation_vector[0]},
+         {static_cast<T>(0),
+          static_cast<T>(1),
+          static_cast<T>(0),
+          translation_vector[1]},
+         {static_cast<T>(0),
+          static_cast<T>(0),
+          static_cast<T>(1),
+          translation_vector[2]},
+         {static_cast<T>(0),
+          static_cast<T>(0),
+          static_cast<T>(0),
+          static_cast<T>(1)}}
+    };
+}
+
 } // namespace transformations
 
 #endif
--- tests/math/transformations.cc
+++ tests/math/transformations.cc
@@ -25,3 +25,33 @@ TEST_CASE("scaling matrix scales a vector")
     CHECK(v[2] == -12.0);
     CHECK(v[3] == 1.0);
 }
+
+TEST_CASE("tranlation matrix moves a vector")
+{
+    mat4 sm = translation_matrix<double>({1.0, 1.0, -1.0});
+    vec4 v = {1.0, 2.0, 3.0, 1.0};
+    v = sm * v;
+    CHECK(v[0] == 2.0);
+    CHECK(v[1] == 3.0);
+    CHECK(v[2] == 2.0);
+    CHECK(v[3] == 1.0);
+}
+
+TEST_CASE("tranlate and scale matrix")
+{
+    mat4 tm = translation_matrix<double>({1.0, 1.0, -1.0});
+    mat4 sm = scaling_matrix(2.0);
+    vec4 v = {1.0, 2.0, 3.0, 1.0};
+    v = tm * sm * v;
+    CHECK(v[0] == 3.0);
+    CHECK(v[1] == 5.0);
+    CHECK(v[2] == 5.0);
+    CHECK(v[3] == 1.0);
+
+    v = {1.0, 2.0, 3.0, 1.0};
+    v = sm * tm * v;
+    CHECK(v[0] == 4.0);
+    CHECK(v[1] == 6.0);
+    CHECK(v[2] == 4.0);
+    CHECK(v[3] == 1.0);
+}
