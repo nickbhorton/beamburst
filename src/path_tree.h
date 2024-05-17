33e3e8778178217ae15d0436c7722df6709cbb7c
Nick Horton <nickkbhorton@gmail.com>
2024-05-17 01:46:45 -0500

    very basic light graph implemented

--- src/path_tree.h
+++ src/path_tree.h
@@ -3,36 +3,131 @@
 
 #include <memory>
 
+#include "array_ops.h"
+#include "camera.h"
+#include "color.h"
 #include "intersectable.h"
+#include "lighting.h"
 #include "line.h"
+
+size_t constexpr max_tree_depth = 10;
+double constexpr enviroment_index_of_refraction = 1.0;
 
 struct Material {
     double index_of_refraction;
+    double reflect_precent;
+    double refract_precent;
 };
 
 struct LightGraphNode {
     // copying line which is 6 doubles
     LightGraphNode(
-        Material const& material,
+        Material const* material,
         size_t depth,
         double intensity,
-        Line const& line
+        Line const& line,
+        LightGraphNode* parent
     )
-        : depth(depth), light_intensity(intensity), line(line),
-          intersection{0, {0, 0, 0}, {}, nullptr}, reflected{}, refracted{},
-          parent{nullptr}, material(&material)
+        : depth(depth), light_intensity(intensity), line(line), intersection{},
+          reflected{}, refracted{}, parent{parent}, material(material)
     {
     }
 
     size_t depth;
     double light_intensity;
     Line line;
-    intersection_t intersection;
+    std::optional<intersection_t> intersection;
     std::unique_ptr<LightGraphNode> reflected;
     std::unique_ptr<LightGraphNode> refracted;
     LightGraphNode* parent;
 
     Material const* material;
+
+    auto construct(
+        std::vector<Intersectable*> const& is,
+        Intersectable const* remove_ptr = nullptr
+    ) -> void
+    {
+        intersection = intersect(is, line, remove_ptr);
+        if (intersection.has_value() && depth < max_tree_depth) {
+            auto const position =
+                solve_line(line, std::get<0>(intersection.value()));
+            if (material->reflect_precent > 0.0) {
+                Line const reflected_line = Line(
+                    position,
+                    reflected_direction(
+                        line.direction,
+                        std::get<1>(intersection.value())
+                    )
+                );
+                reflected = std::make_unique<LightGraphNode>(
+                    material,
+                    depth + 1,
+                    light_intensity * material->reflect_precent,
+                    reflected_line,
+                    this
+                );
+                reflected->construct(is, std::get<3>(intersection.value()));
+            }
+            if (material->refract_precent > 0.0) {
+                Line const refracted_line = Line(
+                    position,
+                    refracted_direction(
+                        line.direction,
+                        std::get<1>(intersection.value()),
+                        parent == nullptr
+                            ? enviroment_index_of_refraction
+                            : parent->material->index_of_refraction,
+                        material->index_of_refraction
+                    )
+                );
+                refracted = std::make_unique<LightGraphNode>(
+                    material,
+                    depth + 1,
+                    light_intensity * material->refract_precent,
+                    refracted_line,
+                    this
+                );
+                refracted->construct(is, std::get<3>(intersection.value()));
+            }
+        }
+    }
+
+    auto calculate_color(Camera const& camera, PointLight const& light) const
+        -> std::array<double, 3>
+    {
+        std::array<double, 3> vcol{0, 0, 0};
+        if (intersection.has_value()) {
+            auto const& [t, normal, uv_opt, iptr] = intersection.value();
+            std::array<double, 3> const position = solve_line(line, t);
+            std::array<double, 3> const view = camera.get_position() - position;
+            double const diffuse =
+                phong_diffuse(light.position, position, normal);
+            double const specular = blin_phong_specular(
+                light.position,
+                position,
+                view,
+                normal,
+                100.0
+            );
+
+            std::array<double, 3> constexpr ambient_color = {0.0, 0.0, 1.0};
+            double constexpr ambient_power = 0.1;
+            double constexpr diffuse_power = 0.5;
+            double constexpr specular_power = 0.4;
+
+            vcol = light_intensity * (specular_power * specular * color::white +
+                                      diffuse_power * diffuse * color::white +
+                                      ambient_power * ambient_color);
+            if (refracted) {
+                vcol = vcol + refracted->calculate_color(camera, light);
+            }
+            if (reflected) {
+                vcol = vcol + reflected->calculate_color(camera, light);
+            }
+        }
+        return vcol;
+    }
 };
 
 struct LightGraph {
--- tests/reflect_refract_test.cc
+++ tests/reflect_refract_test.cc
@@ -1,7 +1,3 @@
-#include <stack>
-
-#include "array_ops.h"
-#include "bvh.h"
 #include "camera.h"
 #include "color.h"
 #include "image.h"
@@ -18,7 +14,7 @@ using namespace linalg;
 
 typedef std::tuple<double, Line, intersection_t> ray_path_segment_t;
 typedef std::vector<std::tuple<double, Line, intersection_t>> ray_path_t;
-typedef std::tuple<std::size_t, std::size_t, ray_path_t> pixel_job_t;
+typedef std::tuple<std::size_t, std::size_t, LightGraphNode> pixel_job_t;
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
@@ -54,44 +50,18 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 
     size_t const height = screen.get_vertical_discretization();
     size_t const width = screen.get_horizontal_discretization();
-    double const light_reflected = 0.5;
-    Material water_material{.index_of_refraction = 1.5};
+    Material water_material{
+        .index_of_refraction = 1.5,
+        .reflect_precent = 0.5,
+        .refract_precent = 0.5,
+    };
 
-    size_t constexpr max_tree_depth = 10;
     for (size_t y = 0; y < height; y++) {
         for (size_t x = 0; x < width; x++) {
-            LightGraphNode root{water_material, 0, 1, camera.get_line_at(x, y)};
-            ray_path_t path{};
-            Line const cam_line = camera.get_line_at(x, y);
-            std::stack<std::tuple<size_t, Line, Intersectable const*>> s{};
-            s.push({0, cam_line, nullptr});
-            while (!s.empty()) {
-                auto const& [depth, line, remove_ptr] = s.top();
-                s.pop();
-                auto const intersection = intersect(is, line, remove_ptr);
-
-                if (intersection.has_value() && depth < max_tree_depth) {
-                    ray_path_segment_t new_segment = {
-                        std::pow(light_reflected, depth),
-                        line,
-                        intersection.value()
-                    };
-                    path.push_back(new_segment);
-                    Line reflected_line = Line(
-                        solve_line(line, std::get<0>(intersection.value())),
-                        reflected_direction(
-                            line.direction,
-                            std::get<1>(intersection.value())
-                        )
-                    );
-                    s.push(
-                        {depth + 1,
-                         reflected_line,
-                         std::get<3>(intersection.value())}
-                    );
-                }
-            }
-            pixel_jobs.push_back({x, y, path});
+            LightGraphNode
+                root{&water_material, 0, 1, camera.get_line_at(x, y), nullptr};
+            root.construct(is);
+            pixel_jobs.push_back({x, y, std::move(root)});
         }
     }
 
@@ -102,48 +72,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
     };
 
     PointLight const light({-1, 1.5, 0});
-    for (const auto& [x, y, path] : pixel_jobs) {
-        vec3 vcol{0, 0, 0};
-        for (auto const& seg : path) {
-            auto const& [intensity, line, intersection] = seg;
-            auto const& [t, normal, uv_opt, iptr] = intersection;
-            vec3 const position = solve_line(line, t);
-            vec3 const view = camera.get_position() - position;
-            double const diffuse =
-                phong_diffuse(light.position, position, normal);
-            double const specular = blin_phong_specular(
-                light.position,
-                position,
-                view,
-                normal,
-                100.0
-            );
-
-            double in_shadow = 1.0;
-            double const distance_to_light =
-                magnitude(light.position - position);
-
-            auto const in_shadow_i = intersect(
-                is,
-                Line(position, normalize(light.position - position)),
-                std::get<3>(intersection)
-            );
-            if (in_shadow_i.has_value() &&
-                std::get<0>(in_shadow_i.value()) > distance_to_light) {
-                in_shadow = 0.0;
-            }
-
-            vec3 constexpr ambient_color = {0.0, 0.0, 1.0};
-            double constexpr ambient_power = 0.1;
-            double constexpr diffuse_power = 0.5;
-            double constexpr specular_power = 0.4;
-
-            vcol = vcol +
-                   intensity *
-                       (in_shadow * specular_power * specular * color::white +
-                        in_shadow * diffuse_power * diffuse * color::white +
-                        ambient_power * ambient_color);
-        }
+    for (const auto& [x, y, light_graph] : pixel_jobs) {
+        vec3 vcol = light_graph.calculate_color(camera, light);
         img.set_color_at(x, y, to_color(vcol));
     }
     img.save("rr.png");
