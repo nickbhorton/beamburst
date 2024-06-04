7440cd2a018f99d56dec4431dadd2d454705e425
Nick Horton <nickkbhorton@gmail.com>
2024-06-04 00:44:22 -0500

    no need for aabbs to inherit from Intersectable

--- src/geometry/aabb.cc
+++ src/geometry/aabb.cc
@@ -1,6 +1,4 @@
 #include "aabb.h"
-#include "intersectable.h"
-#include <optional>
 
 AABB::AABB(
     std::array<double, 3> const& min_point,
@@ -64,64 +62,4 @@ auto AABB::test_intersect(Line const& line) const -> bool
     }
 
     return true;
-}
-
-auto AABB::intersect(
-    Line const& line,
-    [[maybe_unused]] Intersectable const* remove_ptr
-) const -> std::optional<intersection_t>
-{
-    double txmin = (min_point[0] - line.position[0]) / line.direction[0];
-    double txmax = (max_point[0] - line.position[0]) / line.direction[0];
-    if (txmin > txmax) {
-        std::swap(txmin, txmax);
-    }
-
-    double tymin = (min_point[1] - line.position[1]) / line.direction[1];
-    double tymax = (max_point[1] - line.position[1]) / line.direction[1];
-    if (tymin > tymax) {
-        std::swap(tymin, tymax);
-    }
-
-    if ((txmin > tymax) || (tymin > txmax)) {
-        return {};
-    }
-
-    if (tymin > txmin) {
-        txmin = tymin;
-    }
-
-    if (tymax < txmax) {
-        txmax = tymax;
-    }
-
-    double tzmin = (min_point[2] - line.position[2]) / line.direction[2];
-    double tzmax = (max_point[2] - line.position[2]) / line.direction[2];
-    if (tzmin > tzmax) {
-        std::swap(tzmin, tzmax);
-    }
-
-    if ((txmin > tzmax) || (tzmin > txmax))
-        return {};
-
-    if (tzmin > txmin) {
-        txmin = tzmin;
-    }
-    if (tzmax < txmax) {
-        txmax = tzmax;
-    }
-    if (txmin > txmax) {
-        std::swap(txmin, txmax);
-    }
-    intersection_t result =
-        {txmin, {0, 0, 0}, {{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}}, {}, this};
-    return result;
-}
-
-auto AABB::inside_intersect(
-    [[maybe_unused]] Line const& line,
-    [[maybe_unused]] Intersectable const* insider
-) const -> std::optional<intersection_t>
-{
-    return {};
 }
--- src/geometry/aabb.h
+++ src/geometry/aabb.h
@@ -1,10 +1,11 @@
 #ifndef BEAMBURST_AABB_HEADER_
 #define BEAMBURST_AABB_HEADER_
 
-#include "intersectable.h"
+#include "line.h"
 
-// TODO: see if this needs to be inheriting from Intersectable
-class AABB : public Intersectable
+#include <array>
+
+class AABB
 {
 public:
     std::array<double, 3> min_point;
@@ -15,13 +16,6 @@ public:
         std::array<double, 3> const& max_point
     );
 
-    auto
-    intersect(Line const& line, Intersectable const* remove_ptr = nullptr) const
-        -> std::optional<intersection_t>;
-    auto inside_intersect(
-        Line const& line,
-        Intersectable const* inside = nullptr
-    ) const -> std::optional<intersection_t>;
     auto test_intersect(Line const& line) const -> bool;
     auto get_max_extent() const -> std::array<double, 3>;
     auto get_min_extent() const -> std::array<double, 3>;
