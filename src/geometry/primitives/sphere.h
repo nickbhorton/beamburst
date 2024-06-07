7f5bc06e8847871b680d82c9aadca6ce5e477869
Nick Horton <nickkbhorton@gmail.com>
2024-06-06 21:27:45 -0500

    implementing interfaces for sphere

--- src/geometry/primitives/sphere.h
+++ src/geometry/primitives/sphere.h
@@ -5,24 +5,45 @@
 
 class Sphere : public Intersectable
 {
-    std::array<double, 3> position;
-    double radius;
+    std::array<Float, 3> position;
+    Float radius;
 
 public:
-    Sphere(std::array<double, 3> const& position, double radius);
+    Sphere(std::array<Float, 3> const& position, Float radius);
 
-    auto
-    intersect(Line const& line, Intersectable const* remove_ptr = nullptr) const
-        -> std::optional<intersection_t>;
-    auto inside_intersect(Line const& line) const
-        -> std::optional<intersection_t>;
-    auto get_max_extent() const -> std::array<double, 3>;
-    auto get_min_extent() const -> std::array<double, 3>;
+    auto intersect(Line const& line, Float t_max) const
+        -> std::optional<std::unique_ptr<SurfaceIntersection>>;
 
-    friend auto find_intersection(Line const& line, Sphere const& sphere)
-        -> std::optional<double>;
-    friend auto find_both_intersections(Line const& line, Sphere const& sphere)
-        -> std::tuple<std::optional<double>, std::optional<double>>;
+    auto get_max_extent() const -> std::array<Float, 3>;
+    auto get_min_extent() const -> std::array<Float, 3>;
+
+    friend auto find_sphere_intersection(Line const& line, Sphere const& sphere)
+        -> std::optional<Float>;
+};
+
+class SphereSurfaceIntersection : public SurfaceIntersection
+{
+private:
+    Sphere const* primative;
+    Float t;
+    std::array<Float, 3> position;
+    std::array<std::array<Float, 3>, 3> btn_matrix;
+    std::array<Float, 2> uv;
+
+public:
+    SphereSurfaceIntersection(
+        Sphere const* sphere_ptr,
+        Float t,
+        Line const& line,
+        std::array<std::array<Float, 3>, 3> const& btn_matrix,
+        std::array<Float, 2> uv
+    );
+    ~SphereSurfaceIntersection() {}
+    auto generate_ray(std::array<Float, 3> const& direction) const -> Line;
+    auto get_position() const -> std::array<Float, 3>;
+    auto get_uv() const -> std::array<Float, 2>;
+    auto get_btn_matrix() const -> std::array<std::array<Float, 3>, 3>;
+    auto get_t() const -> Float;
 };
 
 #endif
